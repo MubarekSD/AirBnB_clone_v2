3e1f36d341d4d7c267a0b9280dc56a7ba4b5bbf2
Mubarek Seid <mubareksd@gmail.com>
2022-12-21 19:14:13 +0300

    task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""generates a .tgz archive from the contents of the web_static folder
+"""
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """generate
+    """
+    try:
+        local("mkdir versions")
+        res = local("tar -cvxf versions/web_static_{}.tgz"
+                    .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                    capture=True)
+        return res
+    except Exception:
+        return None
