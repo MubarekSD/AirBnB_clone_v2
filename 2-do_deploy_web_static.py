b7994b12e30a789ad62e4de8beb382c3bddb5fdd
Mubarek Seid <mubareksd@gmail.com>
2022-12-22 19:40:24 +0300

    task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,20 +23,20 @@ def do_pack():
 
 
 def do_deploy(archive_path):
+    """Comment
     """
-    """
-    if not os.path.exists(archive_path):
-        return False
-    archived_file = archive_path[9:]
-    newest_version = "/data/web_static/releases/" + archived_file[:-4]
-    archived_file = "/tmp/" + archived_file
-    put(archive_path, "/tmp/")
-    run("mkdir -p {}".format(newest_version))
-    run("tar -xzf {} -c {}/".format(archived_file, newest_version))
-    run("rm {}".format(archived_file))
-    run(" mv {}/web_static/* {}".format(newest_version, newest_version))
-    run("rm -rf {}/web_static".format(newest_version))
-    run("rm -rf /data/web_static/current")
-    run("ln -sf {} /data/web_static/current".format(newest_version))
-    print('New version deployed!')
-    return True
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(newest_version))
+        run("tar -xzf {} -c {}/".format(archived_file, newest_version))
+        run("rm {}".format(archived_file))
+        run(" mv {}/web_static/* {}".format(newest_version, newest_version))
+        run("rm -rf {}/web_static".format(newest_version))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf {} /data/web_static/current".format(newest_version))
+        print('New version deployed!')
+        return True
+    return False
